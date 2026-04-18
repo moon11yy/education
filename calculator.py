4d90824ca3c5ba718bc9894e105e168bac9cdb6e
moon11yy <subaveron@gmail.com>
2026-04-18 17:06:12 +0300

    example

--- calculator.py
+++ calculator.py
@@ -1,6 +1,10 @@
-num1 = input("num1: ")
-num2 = input("num2: ")
+# Преобразуем ввод в целые числа сразу
+num1 = float(input("num1: "))
+num2 = float(input("num2: "))
 
-print ('first number is: ' + num1)
-print ('first number is: ' + num2)
+print('first number is: ' + str(num1))
+print('second number is: ' + str(num2))
 
+numx = num1 * num2
+
+print("Result of subtraction is:", numx)
